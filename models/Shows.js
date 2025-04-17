229ef0453f04b2ca6322919cad97a488106059b6
jaydeepparmar2244 <jaydeep2244.67@gmail.com>
2025-04-17 16:27:28 +0530

    shows-schema-changed

--- models/Shows.js
+++ models/Shows.js
@@ -13,6 +13,11 @@ const ShowsSchema = new Schema(
       ref: "Theatres",
       required: [true, "Theatre ID is required"],
     },
+    screen: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Screens",
+      required: [true, "Screen ID is required"],
+    },
     show_time: {
       type: Date,
       required: [true, "Show time is required"],
@@ -21,6 +26,10 @@ const ShowsSchema = new Schema(
       type: Number,
       required: true,
       min: [1, "There must be at least 1 available seat"],
+    },
+    total_seats: {
+      type: Number,
+      required: true,
     },
     price_per_seat: {
       type: Number,
